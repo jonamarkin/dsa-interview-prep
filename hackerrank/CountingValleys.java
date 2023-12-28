d340af030e8f1f4740460d60c09b955f00aa64ea
Jonathan Ato Markin <jona10711@gmail.com>
2023-12-28 11:29:18 +0100

    Counting valleys and pangram

---
+++ hackerrank/CountingValleys.java
@@ -0,0 +1,36 @@
+package hackerrank;
+
+public class CountingValleys {
+
+    public static void main(String[] args) {
+        System.out.println(countingValleys(5, "DDUUUDDDUDUUUDDDUUU"));
+    }
+
+    public static int countingValleys(int steps, String path) {
+        // Initialize a counter for the sea level and a counter for the valleyCount also
+        // THe sea level will be incremented or decremented depending on what is
+        // encountered in the path
+        // If it's a U incremenet seaLevel by 1, if it's D, decrement seaLevel by -1
+        // Valley start when seaLevel is -1 and ends when seaLevel gets back to 0
+        // Whenever I encounter a U, I check if the current seaLevel is at -1. That
+        // means it is about completing a valley
+
+        int seaLevel = 0;
+        int valleyCount = 0;
+
+        for (Character p : path.toCharArray()) {
+            if (p.equals('U')) {
+
+                if (seaLevel == -1) {
+                    valleyCount++;
+                }
+                seaLevel++;
+            } else if (p.equals('D')) {
+                seaLevel--;
+            }
+        }
+
+        return valleyCount;
+    }
+
+}
---
+++ hackerrank/Pangram.java
@@ -0,0 +1,44 @@
+package hackerrank;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pangram {
+    public static void main(String[] args) {
+
+        System.out.println(pangrams("We promptly judged antique ivory buckles for the next prize"));
+
+    }
+
+    public static String pangrams(String s) {
+        HashMap<Character, Integer> alphabetHashMap = new HashMap<>();
+
+        boolean pangram = true;
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            alphabetHashMap.put(ch, 0);
+        }
+
+        // alphabetHashMap.forEach((key, value) -> System.out.println(key + " - " +
+        // value));
+
+        s = s.replace(" ", "");
+
+        s = s.toLowerCase();
+
+        for (char ch : s.toCharArray()) {
+            alphabetHashMap.put(ch, alphabetHashMap.getOrDefault(ch, 0) + 1);
+
+        }
+
+        for (Map.Entry<Character, Integer> entry : alphabetHashMap.entrySet()) {
+            if (entry.getValue() == 0) {
+                pangram = false;
+                break;
+            }
+        }
+
+        return pangram ? "pangram" : "not pangram";
+
+    }
+}
