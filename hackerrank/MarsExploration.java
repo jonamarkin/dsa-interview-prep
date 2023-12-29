1483b5eca4e91b0f002597d3b09193d8d3f8ea5b
Jonathan Ato Markin <jona10711@gmail.com>
2023-12-29 22:18:04 +0100

    Mars exploration

---
+++ hackerrank/MarsExploration.java
@@ -0,0 +1,31 @@
+package hackerrank;
+
+public class MarsExploration {
+    public static void main(String[] args) {
+
+        System.out.println(marsExploration("SOSTOT"));
+    }
+
+    public static int marsExploration(String s) {
+        // Write your code here
+        // Define the repeating pattern we want to check our string against
+        String sosPattern = "SOS";
+
+        // We want to loop through the string s and make sure it consists of repeating
+        // sequences of sosPattern
+        // We will count the number of wrong letters or changed letters and return them
+        // We can do a simple scan across the charArray of the string
+        // Then we will use modulo to compare the various characters in the string and
+        // the desired pattern
+        // Using modulo will help us cycle through the pattern
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != sosPattern.charAt(i % 3)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
