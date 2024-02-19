b812958ecab3cc4a35268e8f343c0841ab53c566
Jonathan Ato Markin <jona10711@gmail.com>
2024-02-19 10:24:02 +0100

    StringsXOR class created

---
+++ hackerrank/week3/StringsXOR.java
@@ -0,0 +1,9 @@
+package hackerrank.week3;
+
+public class StringsXOR {
+
+    /*
+    With two inputs, XOR is true if and only if the inputs differ (one is true, one is false).
+    With multiple inputs, XOR is true if and only if the number of true inputs is odd.
+     */
+}
