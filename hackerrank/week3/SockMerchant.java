0be2b25117f4aef778840afd8f9094268afbf710
Jonathan Ato Markin <jona10711@gmail.com>
2024-02-21 09:56:31 +0100

    New sckmerchant problem added for week 3

---
+++ hackerrank/week3/SockMerchant.java
@@ -0,0 +1,14 @@
+package hackerrank.week3;
+
+import java.util.List;
+
+public class SockMerchant {
+    public static int sockMerchant(int n, List<Integer> ar) {
+        // Write your code here
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
