4f8208c6e3ab37b9a0b7245311ba2ed59bfb8472
Jonathan Ato Markin <jona10711@gmail.com>
2024-06-14 16:23:27 +0200

    MinAbsDifference

--- hackerrank/week4/MinimumAbsoluteDifference.java
+++ hackerrank/week4/MinimumAbsoluteDifference.java
@@ -1,10 +1,14 @@
 package hackerrank.week4;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MinimumAbsoluteDifference {
     public static void main(String[] args) {
         System.out.println(minimumAbsoluteDifference(List.of(-2,2,4)));
+        System.out.println(minimumAbsoluteDifferenceOpt(Stream.of(-2,2,4).collect(Collectors.toList())));
     }
 
     public static int minimumAbsoluteDifference(List<Integer> arr) {
@@ -16,6 +20,23 @@ public class MinimumAbsoluteDifference {
                 if ( currMinAbsolute <minAbsolute) {
                     minAbsolute = currMinAbsolute;
                 }
+            }
+        }
+
+        return minAbsolute;
+    }
+
+
+    public static int minimumAbsoluteDifferenceOpt(List<Integer> arr) {
+        // Write your code here
+        int minAbsolute = Integer.MAX_VALUE;
+
+        Collections.sort(arr);
+
+        for (int i = 1; i < arr.size(); i++) {
+            int minAbs = Math.abs(arr.get(i) - arr.get(i-1));
+            if (minAbs < minAbsolute){
+                minAbsolute = minAbs;
             }
         }
 
