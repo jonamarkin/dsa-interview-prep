6d22007f414ce78f03ff49c3619bafd993e88658
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-01 17:23:48 +0200

    Optimal solution for count pairs

--- random/CountPairs.java
+++ random/CountPairs.java
@@ -1,13 +1,15 @@
 package random;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CountPairs {
 
     public static void main(String[] args) {
-        List<Integer> list = Stream.of(-1,1,2,3,1).toList();
-        System.out.println(countPairs(list, 2));
+        List<Integer> list = Stream.of(-1,1,2,3,1).collect(Collectors.toList());
+        System.out.println(countPairsOpt(list, 2));
     }
 
     /*
@@ -30,16 +32,29 @@ public class CountPairs {
 
 
     //Better solution
+    /*
+    Use two-pointer solution
+    Sort the array as well
+    Set the two pointers to the extreme ends of the sorted array
+    if the sum is < target. Then it means all pairs in between are also valid
+    Then increment the left pointer to the right
+    Else decrement the right pointer to the left
+     */
     public static int countPairsOpt(List<Integer> nums, int target) {
+        Collections.sort(nums);
         int numPairs = 0;
+        int leftPointer = 0;
+        int rightPointer= nums.size()-1;
 
-        for(int i= 0; i<nums.size()-1; i++){
-            for (int j = i+1; j <nums.size(); j++){
-                if (nums.get(i) +nums.get(j) < target){
-                    numPairs ++;
-                }
+        while (leftPointer < rightPointer){
+            if (nums.get(leftPointer) + nums.get(rightPointer) < target){
+                numPairs += (rightPointer - leftPointer);
+                leftPointer++;
+            }else{
+                rightPointer--;
             }
         }
+
         return numPairs;
     }
 }
