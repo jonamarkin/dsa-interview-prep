812352167f8d4a836cf468489863fd76cd6ce053
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-05 17:58:08 +0200

    All unique characters

---
+++ crackingcodinginterview/arraysandstrings/UniqueCharacters.java
@@ -0,0 +1,29 @@
+package crackingcodinginterview.arraysandstrings;
+
+public class UniqueCharacters {
+    public static void main(String[] args) {
+        System.out.println(allUnique("abcdd"));
+    }
+
+    /*
+    Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+     */
+
+    public static boolean allUnique(String str){
+        boolean[] chars = new boolean[256];
+
+        for (int i=0; i<str.length(); i++){
+            int charInt =  str.charAt(i);
+
+            if(chars[charInt]){
+                return false;
+            }
+
+            chars[charInt] = true;
+        }
+
+        return true;
+    }
+
+
+}
