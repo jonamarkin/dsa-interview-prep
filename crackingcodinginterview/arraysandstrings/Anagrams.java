0cd487d06235694aef0d0b8ab37a65de459ead3d
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-08 15:52:03 +0200

    anagrams

---
+++ crackingcodinginterview/arraysandstrings/Anagrams.java
@@ -0,0 +1,18 @@
+package crackingcodinginterview.arraysandstrings;
+
+
+public class Anagrams {
+
+    /*
+    Write a method to decide if two strings are anagrams or not
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static boolean anagrams(String firstString, String secondString){
+        return true;
+    }
+}
