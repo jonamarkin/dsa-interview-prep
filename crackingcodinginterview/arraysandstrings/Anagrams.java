9f76fa7b87c3ea0403200011a2f0a1ec6a6ffd6a
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-09 01:51:37 +0200

    Valid anagram

--- crackingcodinginterview/arraysandstrings/Anagrams.java
+++ crackingcodinginterview/arraysandstrings/Anagrams.java
@@ -8,11 +8,38 @@ public class Anagrams {
      */
 
     public static void main(String[] args) {
+        String first = "anagram";
+        String second = "nagramc";
 
+        System.out.println(anagrams(first, second));
     }
 
 
     public static boolean anagrams(String firstString, String secondString){
+        int[] charsCount = new int[256];
+
+        if (firstString.length()!= secondString.length()){
+            return false;
+        }
+
+        for (int i = 0; i<firstString.length(); i++){
+            int charInt = firstString.charAt(i);
+            charsCount[charInt]++;
+        }
+
+        for (int i = 0; i<secondString.length(); i++){
+            int charInt = secondString.charAt(i);
+            charsCount[charInt]--;
+        }
+
+        for (int val: charsCount){
+            if (val!=0) return false;
+        }
         return true;
     }
+
+
+    /*
+    I can  also sort the two strings and compare the values at various indexes
+     */
 }
