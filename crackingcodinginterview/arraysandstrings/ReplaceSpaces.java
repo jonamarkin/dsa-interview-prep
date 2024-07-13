7ccd1da5f19f389cf3cb8efcfce9a5f836fd1ef6
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-13 19:56:03 +0200

    Replace strings

--- crackingcodinginterview/arraysandstrings/ReplaceSpaces.java
+++ crackingcodinginterview/arraysandstrings/ReplaceSpaces.java
@@ -7,10 +7,27 @@ public class ReplaceSpaces {
      */
 
     public static void main(String[] args) {
-
+        String input = "Mr John Smith    ";
+        System.out.println(replaceSpaces(input));
     }
 
+    /*
+    Use String builder
+    Create a new string  and append the charcaters in the old one to it, replacing the spaces with %20
+     */
+
     public static String replaceSpaces(String str){
-        return null;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i =0; i<str.length(); i++){
+            if (str.charAt(i)==' '){
+                stringBuilder.append("%20");
+            }else{
+                stringBuilder.append(str.charAt(i));
+            }
+        }
+
+        return stringBuilder.toString();
     }
 }
