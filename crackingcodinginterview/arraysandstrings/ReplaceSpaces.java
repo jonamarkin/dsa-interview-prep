34858c58e070120880867c93dffd6567c48b814b
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-14 17:37:38 +0200

    URIlify a string

--- crackingcodinginterview/arraysandstrings/ReplaceSpaces.java
+++ crackingcodinginterview/arraysandstrings/ReplaceSpaces.java
@@ -8,7 +8,11 @@ public class ReplaceSpaces {
 
     public static void main(String[] args) {
         String input = "Mr John Smith    ";
-        System.out.println(replaceSpaces(input));
+        System.out.println(replaceSpaces(input, 13));
+
+
+        char[] input2 = "Mr John Smith    ".toCharArray();
+        System.out.println(replaceSpacesInPlace(input2, 13));
     }
 
     /*
@@ -16,11 +20,11 @@ public class ReplaceSpaces {
     Create a new string  and append the charcaters in the old one to it, replacing the spaces with %20
      */
 
-    public static String replaceSpaces(String str){
+    public static String replaceSpaces(String str, int trueLength){
 
         StringBuilder stringBuilder = new StringBuilder();
 
-        for(int i =0; i<str.length(); i++){
+        for(int i =0; i<trueLength; i++){
             if (str.charAt(i)==' '){
                 stringBuilder.append("%20");
             }else{
@@ -29,5 +33,43 @@ public class ReplaceSpaces {
         }
 
         return stringBuilder.toString();
+    }
+
+
+    /*
+    Count the number of spaces in the original string
+    Estimate the length of the new string with the addition of the characters to be put in the empty space
+    We are to replace the empty space with three characters
+    Hence since empty character already occupies 1 space, we are supposed to add 2 * the number of empty spaces. That will make up the new string length
+
+    We can then loop in the char array from right to left
+    At each index if it is a space, we put in the new charcters one index at a time
+    Else we put the char there
+     */
+    public static char[] replaceSpacesInPlace(char[] str, int trueLength){
+
+        int spaceCount = 0;
+        for (int i=0; i<trueLength; i++){
+            if (str[i]==' '){
+                spaceCount++;
+            }
+        }
+
+        int newLength = trueLength + spaceCount *2;
+        int insertionIndex = newLength -1;
+
+        for (int i = trueLength-1; i>=0; i--){
+            if (str[i]==' '){
+                str[insertionIndex] = '0';
+                str[insertionIndex-1] = '2';
+                str[insertionIndex - 2] = '%';
+                insertionIndex-=3;
+            }else{
+                str[insertionIndex] = str[i];
+                insertionIndex--;
+            }
+        }
+
+        return str;
     }
 }
