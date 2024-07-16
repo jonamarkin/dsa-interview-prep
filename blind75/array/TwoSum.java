b312a5dfab6901e99e33e8a4c4f7b332d7693002
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-16 20:42:23 +0200

    Two sum problem

--- blind75/array/TwoSum.java
+++ blind75/array/TwoSum.java
@@ -1,5 +1,8 @@
 package blind75.array;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
 
     /*
@@ -11,10 +14,24 @@ public class TwoSum {
      */
 
     public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        int target = 9;
 
+        System.out.println(Arrays.toString(twoSum(arr, target)));
     }
 
     public static int[] twoSum(int[] nums, int target){
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i=0; i<nums.length; i++){
+            int complementary = target - nums[i];
+            if (hashMap.containsKey(complementary)){
+                return new int[]{i, hashMap.get(complementary)};
+            }else{
+                hashMap.put(nums[i], i);
+            }
+        }
 
         return null;
     }
