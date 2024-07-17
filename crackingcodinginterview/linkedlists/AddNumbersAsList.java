dbcf52b5ffc0ccb0f4f1af00e4022f5a344e1321
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-17 13:49:11 +0200

    First solution for adding two linked lists

--- crackingcodinginterview/linkedlists/AddNumbersAsList.java
+++ crackingcodinginterview/linkedlists/AddNumbersAsList.java
@@ -9,9 +9,61 @@ public class AddNumbersAsList {
      */
 
     public static void main(String[] args) {
+        LinkedList firstList = new LinkedList();
+        firstList.addNode(1);
+        firstList.addNode(2);
+        firstList.addNode(3);
 
+        firstList.display();
+
+        LinkedList secondList = new LinkedList();
+
+        secondList.addNode(2);
+        secondList.addNode(4);
+        secondList.addNode(3);
+
+        secondList.display();
+
+        addLists(firstList, secondList);
     }
 
 
+    public static void addLists(LinkedList first, LinkedList second){
+        Node headFirst = first.head;
+        Node headSecond = second.head;
+
+        if (headFirst==null || headSecond==null){
+            return;
+        }
+
+        StringBuilder sumFirst= new StringBuilder(String.valueOf(headFirst.data));
+        StringBuilder sumSecond = new StringBuilder(String.valueOf(headSecond.data));
+
+        while(headFirst.next!=null){
+            sumFirst.append(headFirst.next.data);
+            headFirst = headFirst.next;
+        }
+
+        while(headSecond.next!=null){
+            sumSecond.append(headSecond.next.data);
+            headSecond = headSecond.next;
+        }
+
+        int total  = Integer.parseInt(sumSecond.toString()) + Integer.parseInt(sumFirst.toString());
+        System.out.println(total);
+
+        LinkedList linkedList = new LinkedList();
+
+        String totalString = String.valueOf(total);
+
+        System.out.println(totalString);
+
+        for (int i=0; i<totalString.length(); i++){
+            System.out.println();
+            linkedList.addNode(totalString.charAt(i)-'0');
+        }
+
+        linkedList.display();
+    }
 
 }
