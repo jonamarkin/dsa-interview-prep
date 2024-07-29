67eac84dcade625502c517ab9758f5a0443494c5
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-29 14:24:04 +0200

    TreeNode

---
+++ crackingcodinginterview/treesandgraphs/TreeNode.java
@@ -0,0 +1,17 @@
+package crackingcodinginterview.treesandgraphs;
+
+import com.sun.source.tree.Tree;
+
+public class TreeNode {
+
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(TreeNode left, TreeNode right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode() {
+    }
+}
