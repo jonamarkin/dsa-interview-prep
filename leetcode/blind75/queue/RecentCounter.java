6ad1a6e7b76e10f903ff7a0c265bfca1f3a3da1a
Jonathan Ato Markin <jona10711@gmail.com>
2024-07-31 12:19:32 +0200

    Times Queue

---
+++ leetcode/blind75/queue/RecentCounter.java
@@ -0,0 +1,32 @@
+package leetcode.blind75.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RecentCounter {
+
+    /*
+    You have a RecentCounter class which counts the number of recent requests within a certain time frame.
+
+    Implement the RecentCounter class:
+
+    RecentCounter() Initializes the counter with zero recent requests.
+    int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of requests that has happened in the past 3000 milliseconds (including the new request). Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
+    It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
+     */
+
+    Queue<Integer> timesQueue;
+    public RecentCounter() {
+        timesQueue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        timesQueue.add(t);
+
+        while(!timesQueue.isEmpty() && timesQueue.peek() < t-3000){
+            timesQueue.poll();
+        }
+
+        return timesQueue.size();
+    }
+}
