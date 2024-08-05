1c02f5ecb3d2e237508b21019d5b929b41f4b230
Jonathan Ato Markin <jona10711@gmail.com>
2024-08-05 03:05:54 +0200

    Compress string

--- leetcode/blind75/arraysandstrings/Compress.java
+++ leetcode/blind75/arraysandstrings/Compress.java
@@ -1,4 +1,32 @@
 package leetcode.blind75.arraysandstrings;
 
 public class Compress {
+
+    public int compress(char[] chars) {
+
+        int index = 0;
+        int i = 0;
+
+        while (i <chars.length){
+            char currentChar = chars[i];
+            int count = 0;
+
+            while (i <chars.length && chars[i] ==currentChar){
+                i++;
+                count++;
+            }
+
+            //Write the char
+            chars[index++] = currentChar;
+
+            if (count >1){
+                String countStr = String.valueOf(count);
+                for(char c : countStr.toCharArray()){
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index;
+    }
 }
