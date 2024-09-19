2e8f5a2c9caeeb373f7a0f062407732cbd51f324
Jonathan Ato Markin <jona10711@gmail.com>
2024-09-19 17:17:20 +0200

    Max conseccutive ones in a binary array

---
+++ leetcode/random/MaxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+package leetcode.random;
+
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int count = 0;
+
+        for(int num : nums){
+            if(num==1){
+                count++;
+            }else{
+                maxCount = Math.max(count, maxCount);
+                count=0;
+            }
+        }
+
+        maxCount = Math.max(count, maxCount);
+
+        return maxCount;
+    }
+}
