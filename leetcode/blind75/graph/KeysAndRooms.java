6e610d428a39e5e8a31f695668014073ff198ce8
Jonathan Ato Markin <jona10711@gmail.com>
2024-09-29 17:24:27 +0200

    Can visit all rooms dfs

--- leetcode/blind75/graph/KeysAndRooms.java
+++ leetcode/blind75/graph/KeysAndRooms.java
@@ -44,4 +44,28 @@ public class KeysAndRooms {
 
         return visitedRooms == n;
     }
+
+
+//DFS Solution
+public boolean canVisitAllRooms2(List<List<Integer>> rooms) {
+    boolean[] visited = new boolean[rooms.size()];
+    dfs(rooms, 0, visited);
+
+    for (boolean v: visited){
+        if(!v) return false;
+    }
+
+    return true;
+}
+
+public void dfs(List<List<Integer>> rooms, int room, boolean[] visited){
+        if (visited[room]) return;
+
+        visited[room] = true;
+        for (int key: rooms.get(room)){
+            dfs(rooms, key, visited);
+        }
+}
+
+
 }
