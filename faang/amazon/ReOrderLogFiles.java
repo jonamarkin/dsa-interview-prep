bbf2cfc1b55f6449e92fbf909d2a2612f281b1bf
Jonathan Ato Markin <jona10711@gmail.com>
2024-10-02 16:29:57 +0200

    Reorder log files question added

--- faang/amazon/ReOrderLogFiles.java
+++ faang/amazon/ReOrderLogFiles.java
@@ -3,6 +3,21 @@ package faang.amazon;
 import java.util.*;
 
 public class ReOrderLogFiles {
+
+    /*
+    You are given an array of logs. Each log is a space-delimited string of words, where the first word is the identifier.
+
+    There are two types of logs:
+
+    Letter-logs: All words (except the identifier) consist of lowercase English letters.
+    Digit-logs: All words (except the identifier) consist of digits.
+    Reorder these logs so that:
+
+    The letter-logs come before all digit-logs.
+    The letter-logs are sorted lexicographically by their contents. If their contents are the same, then sort them lexicographically by their identifiers.
+    The digit-logs maintain their relative ordering.
+    Return the final order of the logs.
+     */
     public String[] reorderLogFiles(String[] logs) {
 
         List<String> letterLogs = new ArrayList<>();
