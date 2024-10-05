2e194b679a6289b63197859e5786935849991ee6
Jonathan Ato Markin <jona10711@gmail.com>
2024-10-05 20:27:31 +0200

    num set bits

--- random/bitmanipulation/CountNumOfSetBits.java
+++ random/bitmanipulation/CountNumOfSetBits.java
@@ -16,7 +16,21 @@ public class CountNumOfSetBits {
         return count;
     }
 
+
+    public static int numberOfSetBits2(int n){
+        int count = 0;
+
+        while(n>1){
+            count+= n&1;
+            n = n>>1;
+        }
+
+        if (n==1) count+=1;
+
+        return count;
+    }
+
     public static void main(String[] args) {
-        System.out.println(numberOfSetBits(13));
+        System.out.println(numberOfSetBits2(13));
     }
 }
