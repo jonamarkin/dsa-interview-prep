942c614bb3011a04af7445d0199a4584559dfc39
Jonathan Ato Markin <jona10711@gmail.com>
2024-10-05 17:12:31 +0200

    Single number

---
+++ faang/amazon/SingleNumber.java
@@ -0,0 +1,22 @@
+package faang.amazon;
+
+import java.util.HashMap;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+
+        for (int num : map.keySet()){
+            if(map.get(num)==1){
+                return num;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ leetcode/blind75/backtracking/LetterCombinations.java
@@ -0,0 +1,11 @@
+package leetcode.blind75.backtracking;
+
+import java.util.List;
+
+public class LetterCombinations {
+
+    public List<String> letterCombinations(String digits) {
+
+        return null;
+    }
+}
