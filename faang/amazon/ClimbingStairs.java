e14c6d1a14fbf9abc07a013c114156bc1b8284d3
Jonathan Ato Markin <jona10711@gmail.com>
2024-10-05 15:26:02 +0200

    Climb stairs

--- faang/amazon/ClimbingStairs.java
+++ faang/amazon/ClimbingStairs.java
@@ -33,4 +33,21 @@ public class ClimbingStairs {
 
         return climbStairs2(n-1) + climbStairs2(n-2);
     }
+
+
+    public int climbStairs3(int n) {
+        if (n<=1){
+            return 1;
+        }
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] =1;
+
+        for (int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
 }
