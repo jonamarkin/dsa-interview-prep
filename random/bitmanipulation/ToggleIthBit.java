2cfd838490e431ac58ad79fe1c830e744dfc79fe
Jonathan Ato Markin <jona10711@gmail.com>
2024-10-05 20:06:43 +0200

    Toggle the ith bit

---
+++ random/bitmanipulation/ToggleIthBit.java
@@ -0,0 +1,13 @@
+package random.bitmanipulation;
+
+public class ToggleIthBit {
+    public static int setIthBit(int n, int i){
+
+
+        return n ^ (1<<i);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(setIthBit(9, 2));
+    }
+}
