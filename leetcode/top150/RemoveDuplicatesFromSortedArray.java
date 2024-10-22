1e07afde9f4eea0c13bd57889c86e83af3ceff52
Jonathan Ato Markin <jona10711@gmail.com>
2024-10-23 00:02:41 +0200

    Remove duplicates from sorted array

---
+++ leetcode/top150/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+package leetcode.top150;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int writeIndex =1;
+
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]!=nums[writeIndex-1]){
+                nums[writeIndex] = nums[i];
+                writeIndex++;
+            }
+        }
+
+        return writeIndex;
+    }
+}
