9bbc7a02cd7cc1ced4643b44f1f012bcc8c39839
Jonathan Ato Markin <jona10711@gmail.com>
2024-10-25 18:23:16 +0200

    Majority element

---
+++ leetcode/top150/MajorityElement.java
@@ -0,0 +1,26 @@
+package leetcode.top150;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+
+        Map<Integer, Integer> numFreq = new HashMap<>();
+        int n = nums.length;
+        int floorVal = n/2;
+
+        for(int num: nums){
+            numFreq.put(num, numFreq.getOrDefault(num, 0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry: numFreq.entrySet()){
+            if(entry.getValue() > floorVal){
+                return entry.getKey();
+            }
+        }
+
+        return 0;
+    }
+}
