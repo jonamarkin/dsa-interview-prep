d6745cf9260f35b276fb7d6ae840b06667e2c4b9
Jonathan Ato Markin <jona10711@gmail.com>
2024-11-08 12:13:34 +0100

    Rotate array second solution

--- leetcode/top150/RotateArray.java
+++ leetcode/top150/RotateArray.java
@@ -18,5 +18,28 @@ public class RotateArray {
     }
 
 
+    public void rotate2(int[] nums, int k) {
+        int n = nums.length;
+
+        k = k%n;
+
+        reverseArr(nums, 0, n-1);
+
+        reverseArr(nums, 0, k-1);
+
+        reverseArr(nums, k, n-1);
+    }
+
+    private void reverseArr(int[] nums, int start, int end) {
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
 
 }
