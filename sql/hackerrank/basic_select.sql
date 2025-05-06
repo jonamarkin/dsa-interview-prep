f9a9e9b85fa62d86555807a063854d851111c7c6
Jonathan Ato Markin <jona10711@gmail.com>
2025-05-06 23:53:22 +0200

    Files renamed

--- sql/hackerrank/basic_select.sql
+++ sql/hackerrank/basic_select.sql
@@ -69,3 +69,8 @@ SELECT DISTINCT CITY FROM STATION
 WHERE (CITY NOT LIKE 'A%' AND CITY NOT LIKE 'E%' AND CITY NOT LIKE 'I%' AND CITY NOT LIKE 'O%' AND CITY NOT LIKE 'U%')
    OR (CITY NOT LIKE '%A' AND CITY NOT LIKE '%E' AND CITY NOT LIKE '%I' AND CITY NOT LIKE '%O' AND CITY NOT LIKE '%U')
 
+
+-- Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT Name from STUDENTS
+where Marks>75
+Order by RIGHT(Name, 3), ID ASC;
