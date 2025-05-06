3fb89615163f9efd1b72f0acb44e6f063b1a8b98
Jonathan Ato Markin <jona10711@gmail.com>
2025-05-06 23:16:53 +0200

    quer

--- sql/hackerrank.sql
+++ sql/hackerrank.sql
@@ -34,3 +34,37 @@ WHERE CITY LIKE 'A%'
    OR CITY LIKE 'I%'
    OR CITY LIKE 'O%'
    OR CITY LIKE 'U%'
+
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY LIKE '%A'
+   OR CITY LIKE '%E'
+   OR CITY LIKE '%I'
+   OR CITY LIKE '%O'
+   OR CITY LIKE '%U'
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%')
+AND (CITY LIKE '%A' OR CITY LIKE '%E' OR CITY LIKE '%I' OR CITY LIKE '%O' OR CITY LIKE '%U')
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[aeiouAEIOU].*[aeiouAEIOU]$';
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY NOT LIKE 'A%'
+   AND CITY NOT LIKE 'E%'
+   AND CITY NOT LIKE 'I%'
+   AND CITY NOT LIKE 'O%'
+   AND CITY NOT LIKE 'U%'
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP '^[aeiouAEIOU]';
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE (CITY NOT LIKE 'A%' AND CITY NOT LIKE 'E%' AND CITY NOT LIKE 'I%' AND CITY NOT LIKE 'O%' AND CITY NOT LIKE 'U%')
+   OR (CITY NOT LIKE '%A' AND CITY NOT LIKE '%E' AND CITY NOT LIKE '%I' AND CITY NOT LIKE '%O' AND CITY NOT LIKE '%U')
