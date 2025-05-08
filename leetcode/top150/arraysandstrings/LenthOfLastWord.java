9029beb535883208851017e8fc852d976e1d4ead
Jonathan Ato Markin <jona10711@gmail.com>
2025-05-08 11:45:27 +0200

    Added problem description to len of last wormd question

--- leetcode/top150/arraysandstrings/LenthOfLastWord.java
+++ leetcode/top150/arraysandstrings/LenthOfLastWord.java
@@ -1,6 +1,11 @@
 package leetcode.top150.arraysandstrings;
 
 public class LenthOfLastWord {
+    /*
+    Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+    A word is a maximal substring consisting of non-space characters only.
+     */
 
     public int lengthOfLastWord(String s) {
         // String[] arr = s.split(" ");
