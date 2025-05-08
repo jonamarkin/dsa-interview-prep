10cf5e5cd0b9f07594a490aa5ebc2f80dbd64ea2
Jonathan Ato Markin <jona10711@gmail.com>
2025-05-08 12:49:33 +0200

    First occurrence of string

---
+++ leetcode/top150/arraysandstrings/FirstOccurrenceOfString.java
@@ -0,0 +1,37 @@
+package leetcode.top150.arraysandstrings;
+
+public class FirstOccurrenceOfString {
+
+    /*
+    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+    Example 1:
+
+    Input: haystack = "sadbutsad", needle = "sad"
+    Output: 0
+    Explanation: "sad" occurs at index 0 and 6.
+    The first occurrence is at index 0, so we return 0.
+    Example 2:
+
+    Input: haystack = "leetcode", needle = "leeto"
+    Output: -1
+    Explanation: "leeto" did not occur in "leetcode", so we return -1.
+     */
+
+    public int strStr(String haystack, String needle) {
+
+        if(needle.length()==0 || needle==null) return 0;
+
+        int nLen = needle.length();
+        int hLen = haystack.length();
+
+        for(int i=0; i<=hLen-nLen; i++){
+            if(haystack.substring(i, i+nLen).equals(needle)){
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+}
