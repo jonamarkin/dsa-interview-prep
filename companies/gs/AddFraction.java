7a7f74f15376ac76f175b0355c25e9f001d06dbf
Jonathan Ato Markin <jona10711@gmail.com>
2025-07-08 13:50:49 +0200

    Add fractions

--- companies/gs/AddFraction.java
+++ companies/gs/AddFraction.java
@@ -9,24 +9,46 @@ public class AddFraction {
 
     public static int[] addFractions(int[] fraction1, int[] fraction2) {
         // Extract numerator and denominator from the input arrays
-        int numerator1 = fraction1[0];
-        int denominator1 = fraction1[1];
-        int numerator2 = fraction2[0];
-        int denominator2 = fraction2[1];
 
-        // Calculate the common denominator
-        int commonDenominator = denominator1 * denominator2;
+        int num1 = fraction1[0];
+        int den1 = fraction1[1];
+        int num2 = fraction2[0];
+        int den2 = fraction2[1];
 
-        // Calculate the new numerators
-        int newNumerator1 = numerator1 * denominator2;
-        int newNumerator2 = numerator2 * denominator1;
+        if(den1== 0 || den2 ==0){
+            throw new IllegalArgumentException("Denominator cannot be zero");
+        }
 
-        // Add the numerators
-        int resultNumerator = newNumerator1 + newNumerator2;
+        int newDen = den1 * den2;
+        int newNum = num1 * den2 + num2 * den1;
 
-        // Return the result as an array
-        return new int[]{resultNumerator, commonDenominator};
+        int gcd = calcGCD(newNum, newDen);
 
+        return new int[]{newNum/gcd, newDen/gcd};
+
+    }
+
+    private static int calcGCD(int a, int b) {
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        while(b!=0){
+            int temp = b;
+            b = a%b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] fraction1 = {1, 2};
+        int[] fraction2 = {1, 3};
+
+        int[] result = addFractions(fraction1, fraction2);
+
+        System.out.println(result[0] + "/" + result[1]);
     }
 
 }
