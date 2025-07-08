6d494097cfa318dfff267e61d72ab718cb35408a
Jonathan Ato Markin <jona10711@gmail.com>
2025-07-08 18:30:49 +0200

    Array grouping

--- companies/gs/ArrayGrouping.java
+++ companies/gs/ArrayGrouping.java
@@ -21,9 +21,24 @@ public class ArrayGrouping {
         }
     }
 
+    public static void stableGroupPosNeg(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] < 0){
+                int j = i;
+                while(j > 0 && arr[j-1] > 0){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    j--;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {-1, 2, -3, 4, -5, 6, 7, -8, 9};
-        groupPosNegInPlace(arr);
+        //groupPosNegInPlace(arr);
+        stableGroupPosNeg(arr);
         for (int i: arr){
             System.out.print(i + " ");
         }
