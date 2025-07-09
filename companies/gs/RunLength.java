a3b90c854fe159afe9594183b4adcc6286e65128
Jonathan Ato Markin <jona10711@gmail.com>
2025-07-09 22:59:39 +0200

    run length encoding

---
+++ companies/gs/RunLength.java
@@ -0,0 +1,31 @@
+package companies.gs;
+
+public class RunLength {
+
+    public static String encode(String input){
+
+        if(input ==null || input.isEmpty()){
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        char prevChar = input.charAt(0);
+
+        for(int i=1; i<input.length(); i++){
+            char currentChar = input.charAt(i);
+
+            if(currentChar == prevChar){
+                count++;
+            }else{
+                sb.append(count).append(prevChar);
+                count = 1;
+                prevChar = currentChar;
+            }
+        }
+
+        sb.append(count).append(prevChar);
+
+        return sb.toString();
+    }
+}
